70d63a7ce93c4b51f769ab1f46df1f338d4ed595
max.leuthaeuser@googlemail.com <max.leuthaeuser@googlemail.com@a456bc7f-0a70-c31a-b790-be563d186878>
2011-09-24 12:22:38 +0000

    - parallel profiling of multiple sensor values is possible now, provide them separated with "+"

--- modules/NaoProfilingModule.py
+++ modules/NaoProfilingModule.py
@@ -3,6 +3,7 @@ import uuid
 import time
 import threading
 from naoqi import ALProxy
+from threading import Thread
 
 app = bottle.Bottle()
 
@@ -15,28 +16,44 @@ processes = {}
 # sensor values of the Nao through NaoQi
 memProxy = ALProxy("ALMemory", "localhost", 9559)
 
+ONE = 0
+MULT = 1
+
 @app.route('/')
 @app.route('/index.html')
 def index():
 	return "This module has no web interface."
 
+def profile(args):
+		pointer = args['pointer']
+		sensor_value = args['sensor_values'] 
+		pointer.append({'timestamp':"%.6f" % time.time(), 'value':str(memProxy.getData(sensor_value))})
+
 @app.route('/start/:interval/:sensor#.+#')
 def start(interval=100, sensor=""):
 	'''
 	@param interval: in ms
-	@param sensor: the sensor you want to use for profiling
+	@param sensor: the sensor you want to use for profiling, use '+' to provide multiple sensors
 	@return: an unique ID for your profiling process. Use this ID to stop
 		the profiling using method 'stop'
 	'''
 	p_id = str(uuid.uuid4())
-	list = []
-	def profile(args):
-		pointer = args['pointer']
-		sensor_value = args['sensor_values'] 
-		pointer.append({'timestamp':"%.6f" % time.time(), 'value':str(memProxy.getData(sensor_value))})
-	worker = StoppableWorker(profile, {'pointer':list, 'sensor_values':sensor}, int(interval))
-	processes[p_id] = (worker, list)
-	worker.start()	
+	if sensor.find('+') != -1:
+		sensors = sensor.split('+')
+		mapping = {}
+		workerlist = []
+		for s in sensors:
+			list = []
+			mapping[s] = list
+			worker = StoppableWorker(profile, {'pointer':list, 'sensor_values':s}, int(interval))
+			workerlist.append(worker)
+			worker.start()
+		processes[p_id] = (MULT, workerlist, mapping)
+	else:	
+		list = []
+		worker = StoppableWorker(profile, {'pointer':list, 'sensor_values':sensor}, int(interval))
+		processes[p_id] = (ONE, worker, list)
+		worker.start()	
 	return p_id
 
 @app.route('/stop/:id')
@@ -45,12 +62,29 @@ def stop(id=""):
 	@param id: the ID of the profiling job you want to stop 
 	@return: an JSON object containing the results with time stamps for each result
 		It will be empty if the given ID is unknown or if there was not enough time between start and stop to profile anything.
-		Format will be: {result:[{timestamp, value}*]}
+		Format will be: {result:[{timestamp, value}*]} or the name of the sensor instead of 'result' if you have started the profiling
+		with multiple sensors, i.e.: {name:[{timestamp, value}*]*}
 	'''
+	def cleanup(workerlist):
+		for w in workerlist:
+			w.join()
+
 	if id in processes.keys():
-		worker = (processes[id])[0]
-		worker.stop()
-		return {'result':(processes[id])[1]}
+		if (processes[id])[0] == ONE:
+			worker = (processes[id])[1]
+			worker.stop()
+			worker.join()
+			return {'result':(processes[id])[2]}
+		else:
+			result = {}
+			workerlist = (processes[id])[1]
+			for w in workerlist:
+				w.stop()
+			Thread(target=cleanup, args=(workerlist,)).start()
+			resultmap = (processes[id])[2]
+			for r in resultmap.keys():
+				result[r] = resultmap[r]
+			return result
 	else:
 		return {}
 
